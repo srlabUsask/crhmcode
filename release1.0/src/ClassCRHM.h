6ea19f683f1fd392ec9fbb4a1f0ceac457ee30be
Justin Schneider <jhs507@mail.usask.ca>
2020-09-10 14:53:59 -0600

    Added defalult initalization to Classlog member variables.

--- release1.0/src/ClassCRHM.h
+++ release1.0/src/ClassCRHM.h
@@ -493,10 +493,10 @@ public:
 
 class Classlog : Classfilter {
 public:
-	long delay;
-	long duration;
-	double A;
-	double B;
+	long delay {0};
+	long duration {0};
+	double A {0.0};
+	double B {0.0};
 	Classlog(ClassData *MyObs, string ToVar, string args, string argtypes = "CCCC");
 
 	virtual void doFunc(long Obs, long Line);
