8a8331a17349f722fcbd54577f21d27045d9b658
Justin Schneider <jhs507@mail.usask.ca>
2020-09-11 12:11:57 -0600

    Added initalization defaults for  Myparser membe variables.

--- release1.0/src/ClassModule.h
+++ release1.0/src/ClassModule.h
@@ -321,14 +321,15 @@ public:
 class Myparser {
 	const char *exp_ptr;  // points to the expression
 	char token[80]; // holds current token
-	char tok_type;  // holds token's type
+	char tok_type {'\0'};  // holds token's type
 
-	int row, col;
-	int repeat;
-	int rowrepeat;
-	int Bang;
-	bool repeatset;
-	ClassPar *LocalPar;
+	int row {0};
+	int col {0};
+	int repeat {0};
+	int rowrepeat {0};
+	int Bang {0};
+	bool repeatset {false};
+	ClassPar* LocalPar {NULL};
 
 	void eval_exp2(double &result);
 	void eval_exp3(double &result);
