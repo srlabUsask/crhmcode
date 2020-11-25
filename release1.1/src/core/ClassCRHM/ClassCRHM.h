b0f957d28cb5b113ef55f315a36e9c1236d6ba45
jhs507 <jhs507@mail.usask.ca>
2020-11-25 16:05:09 -0600

    Extracted class ClassPar from ClassCRHM.h and ClassCRHM.cpp into ClassPar.h and ClassPar.cpp.

--- release1.1/src/core/ClassCRHM/ClassCRHM.h
+++ release1.1/src/core/ClassCRHM/ClassCRHM.h
@@ -12,6 +12,7 @@ using namespace std;
 #include "TStringList/TStringList.h"
 #include "ClassData.h"
 #include "ClassVar.h"
+#include "ClassPar.h"
 #include <string>
 #include <iostream>
 #include <map>
@@ -28,83 +29,6 @@ typedef unsigned long Word;
 typedef int INT;
 
 
-class ClassPar {
-public:
-	string module;
-	string param;
-	string basemodule;
-	string help;
-	string units;
-
-	TVar varType;
-	TDim dimen { TDim::NDEFNZ};
-
-	long dim;
-	long lay {0};
-	long variation_set {0};
-
-	double *values;
-	long *ivalues;
-
-	double ** layvalues;
-	long **ilayvalues;
-
-	string valstr;
-	double minVal {0.0};
-	double maxVal {0.0};
-
-	TStringList* Strings {NULL};
-
-	TVISIBLE visibility { TVISIBLE::USUAL};
-
-	long Inhibit_share; // 0 - shared, 1 - linked to declparam, 2 - declputparam unlinked
-
-						// backup storage
-
-	double** layvaluesBkup {NULL};
-	long** ilayvaluesBkup {NULL};
-
-	ClassPar *Identical;
-
-	TStringList* StringsBkup {NULL};
-
-	ClassPar(string module = "none", string param = "none")
-		: module(module), param(param), dim(0), visibility(TVISIBLE::USUAL), variation_set(0), varType(TVar::none),
-		values(NULL), ivalues(NULL), layvalues(NULL), ilayvalues(NULL), Identical(NULL), Inhibit_share(0) {};
-
-
-
-
-	ClassPar(string module, string param, TDim dimen,  // strings
-		string CommaText, string help, TVar varType, int Grpdim);
-
-	ClassPar(string module, string param, TDim dimen,  // integer and double
-		string valstr, double minVal, double maxVal,
-		string help, string units, TVar varType, int defdim = 0, int GrpN = 0);
-
-	ClassPar(ClassPar &p);  // copy constructor
-
-	double &operator[](int ii) { return values[ii]; }
-
-	virtual  ~ClassPar();
-
-	bool Same(ClassPar &p);  // compares parameter data
-
-	void Change(ClassPar &p);  // changes this values to p
-
-	virtual string UserName(void) { return module + ' ' + param; };
-
-	friend class ::parser;
-
-	void ExpandShrink(long new_dim);
-
-	void BackUp(void);
-
-	void Restore(void);
-
-	long GetUnit(void);
-
-};
 
 class Classfilter {
 public:
---
+++ release1.1/src/core/ClassCRHM/ClassPar.h
@@ -0,0 +1,107 @@
+#pragma once
+
+#ifndef CLASSPAR
+#define CLASSPAR
+
+#include "ClassCRHM.h"
+
+using namespace std;
+
+class parser;
+
+class ClassPar {
+public:
+	string module;
+	string param;
+	string basemodule;
+	string help;
+	string units;
+
+	TVar varType;
+	TDim dimen{ TDim::NDEFNZ };
+
+	long dim;
+	long lay{ 0 };
+	long variation_set{ 0 };
+
+	double* values;
+	long* ivalues;
+
+	double** layvalues;
+	long** ilayvalues;
+
+	string valstr;
+	double minVal{ 0.0 };
+	double maxVal{ 0.0 };
+
+	TStringList* Strings{ NULL };
+
+	TVISIBLE visibility{ TVISIBLE::USUAL };
+
+	long Inhibit_share; // 0 - shared, 1 - linked to declparam, 2 - declputparam unlinked
+
+						// backup storage
+
+	double** layvaluesBkup{ NULL };
+	long** ilayvaluesBkup{ NULL };
+
+	ClassPar* Identical;
+
+	TStringList* StringsBkup{ NULL };
+
+	ClassPar(string module = "none", string param = "none") : 
+		module(module), 
+		param(param), 
+		dim(0), 
+		visibility(TVISIBLE::USUAL), 
+		variation_set(0), 
+		varType(TVar::none),
+		values(NULL), 
+		ivalues(NULL), 
+		layvalues(NULL), 
+		ilayvalues(NULL), 
+		Identical(NULL), 
+		Inhibit_share(0) 
+	{};
+
+	ClassPar(string module, string param, TDim dimen, string CommaText, string help, TVar varType, int Grpdim);
+
+	ClassPar(
+		string module, 
+		string param, 
+		TDim dimen, 
+		string valstr, 
+		double minVal, 
+		double maxVal,
+		string help, 
+		string units, 
+		TVar varType, 
+		int defdim = 0, 
+		int GrpN = 0
+	);
+
+	ClassPar(ClassPar& p);  // copy constructor
+
+	double& operator[](int ii) { return values[ii]; }
+
+	virtual  ~ClassPar();
+
+	bool Same(ClassPar& p);  // compares parameter data
+
+	void Change(ClassPar& p);  // changes this values to p
+
+	virtual string UserName(void) { return module + ' ' + param; };
+
+	friend class ::parser;
+
+	void ExpandShrink(long new_dim);
+
+	void BackUp(void);
+
+	void Restore(void);
+
+	long GetUnit(void);
+
+};
+
+#endif // !CLASSPAR
