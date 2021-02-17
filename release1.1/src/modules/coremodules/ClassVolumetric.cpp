a15be95cb6b26067c7c7aa617184e30c93b32b54
jhs507 <jhs507@mail.usask.ca>
2021-02-17 14:37:59 -0600

    Modified ClassVolumetric to match crhm release 07/02/2020

--- release1.1/src/modules/coremodules/ClassVolumetric.cpp
+++ release1.1/src/modules/coremodules/ClassVolumetric.cpp
@@ -14,85 +14,104 @@
 
 using namespace CRHM;
 
-ClassVolumetric* ClassVolumetric::klone(string name) const{
-  return new ClassVolumetric(name);
+ClassVolumetric* ClassVolumetric::klone(string name) const {
+    return new ClassVolumetric(name);
 }
 
 void ClassVolumetric::decl(void) {
 
-  Description = "'Converts soil moisture to volumetric equivalent and determines fall status.'";
+    Description = "'Converts soil moisture to volumetric equivalent and determines fall status.'";
 
-  declvar("Volumetric", TDim::NHRU, "volumetric soil moisture", "()", &Volumetric);
+    declvar("Volumetric", TDim::NHRU, "volumetric soil moisture", "()", &Volumetric);
 
-  declvar("fallstat_V", TDim::NHRU, "fallstat_V copy of parameter fallstat", "()", &fallstat_V);
+    declvar("Volumetric_rechr", TDim::NHRU, "volumetric soil moisture in soil recharge zone", "()", &Volumetric_rechr); // 04/14/2020: adding option for updating Si or fallstat based on Volumetric moisture content in recharge layer
+    
+    declvar("fallstat_V", TDim::NHRU, "fallstat_V copy of parameter fallstat", "()", &fallstat_V);
 
-  declvar("Si_V", TDim::NHRU, "Si_V copy of parameter Si", "()", &Si_V);
+    declvar("Si_V", TDim::NHRU, "Si_V copy of parameter Si", "()", &Si_V);
 
-  declparam("soil_type", TDim::NHRU, "[4]", "0", "12",
+    declparam("soil_type", TDim::NHRU, "[4]", "0", "12",
         "water/sand/loamsand/sandloam/loam/siltloam/sasclloam/clayloam/siclloam/sandclay/siltclay/clay/pavement" //
         " 0 - 12", "()", &soil_type);
 
-  declparam("soil_moist_max", TDim::NHRU, "[375.0]", "0.0", "5000.0",
-    "Maximum available water holding capacity of soil profile."//
-    "Soil profile is surface to bottom of rooting zone",
-    "(mm)", &soil_moist_max);
+    declparam("soil_moist_max", TDim::NHRU, "[375.0]", "0.0", "5000.0",
+        "Maximum available water holding capacity of soil profile."//
+        "Soil profile is surface to bottom of rooting zone",
+        "(mm)", &soil_moist_max);
 
-  declparam("soil_Depth", TDim::NHRU, "[1.0]", "0.0", "10.0", "depth of soil column", "(m)", &soil_Depth);
+    declparam("soil_rechr_max", TDim::NHRU, "[60.0]", "0.0", "350.0",
+        " Maximum available water holding capacity for soil recharge zone (upper portion of soil_moist where losses occur as both evaporation "//   // 04/14/2020
+        "and transpiration).  Must be less than or equal to soil_moist.", "(mm)", &soil_rechr_max);
 
-  declparam("Si_correction", TDim::NHRU, "[0.0]", "0.0", "1.0", "Si correction number", "()", &Si_correction);
+    declparam("soil_Depth", TDim::NHRU, "[1.0]", "0.0", "10.0", "depth of soil column", "(m)", &soil_Depth);
 
-  declparam("set_fallstat", TDim::NHRU, "[305]", "0","366", "set fallstat on this Julian date", "()", &set_fallstat);
+    declparam("Si_correction", TDim::NHRU, "[0.0]", "0.0", "1.0", "Si correction number", "()", &Si_correction);
 
-  declparam("Si", TDim::NHRU, "[0.5]", "0.0", "1.0", "initial soil saturation (0 - 0.4m)", "(mm^3/mm^3)", &Si);
+    declparam("set_fallstat", TDim::NHRU, "[305]", "0", "366", "set fallstat on this Julian date", "()", &set_fallstat);
 
-//  declputparam("*", "Si", "(mm^3/mm^3)", &Si);
+    declparam("Volumetric_option", TDim::NHRU, "[1]", "0", "1", "option of setting Si or fallstat: 0 - based on Volumetric moisture content in entire soil column, 1 - based on Volumetric moisture content in soil recharge layer", "()", &Volumetric_option);  // 04/14/2020:
 
+    declputparam("*", "Si", "(mm^3/mm^3)", &Si);
 
-  declputparam("*", "fallstat",  "(%)", &fallstat);
+    declputparam("*", "fallstat", "(%)", &fallstat);
 
-  declgetvar("*", "soil_moist", "(mm)", &soil_moist);
+    declgetvar("*", "soil_moist", "(mm)", &soil_moist);
+
+    declgetvar("*", "soil_rechr", "(mm)", &soil_rechr);
 
 }
 
 void ClassVolumetric::init(void) {
 
-  for(hh = 0; chkStruct(); ++hh)
-    fallstat_V[hh] = 0.0;
+    for (hh = 0; chkStruct(); ++hh)
+        fallstat_V[hh] = 0.0;
 }
 
 void ClassVolumetric::run(void) {
 
-  long nstep = getstep() % Global::Freq;
-  long Julian = julian("now"); // same as "decday"
+    long nstep = getstep() % Global::Freq;
+    long Julian = julian("now"); // same as "decday"
 
-  for(hh = 0; chkStruct(); ++hh) {
+    for (hh = 0; chkStruct(); ++hh) {
 
-    Volumetric[hh] = (soil_moist[hh]/soil_Depth[hh] + SetSoilproperties[soil_type[hh]][1])/1000.0;
+        Volumetric[hh] = (soil_moist[hh] / soil_Depth[hh] + SetSoilproperties[soil_type[hh]][1]) / 1000.0;
 
-    if(nstep == 0 && set_fallstat[hh] == Julian || (getstep() == 1 && Julian > set_fallstat[hh])){
-      if(Si){
-        double X = 1.0;
-        if(SetSoilproperties[soil_type[hh]][3] > 0.0){
-          X = Volumetric[hh]/SetSoilproperties[soil_type[hh]][3]*1000.0;
-          X = X - Si_correction[hh];
+        Volumetric_rechr[hh] = (soil_rechr[hh] / soil_rechr_max[hh] * SetSoilproperties[soil_type[hh]][3] + SetSoilproperties[soil_type[hh]][1]) / 1000.0;  // 04/14/2020
+
+        if (nstep == 0 && set_fallstat[hh] == Julian || (getstep() == 1 && Julian > set_fallstat[hh])) {
+            if (Si) {
+                double X = 1.0;
+                if (SetSoilproperties[soil_type[hh]][3] > 0.0) {
+                    if (Volumetric_option[hh])  // 04/14/2020
+                        X = Volumetric_rechr[hh] / SetSoilproperties[soil_type[hh]][3] * 1000.0;
+                    else
+                        X = Volumetric[hh] / SetSoilproperties[soil_type[hh]][3] * 1000.0;
+                    X = X - Si_correction[hh];
+                }
+
+                if (X < 0.0) {
+                    X = 0.0;
+                } 
+                else if (X > 1.0) {
+                    X = 1.0;
+                }
+
+                const_cast<double*> (Si)[hh] = X;
+                Si_V[hh] = Si[hh];
+                
+            }
+
+            if (fallstat[hh]) {
+                if (SetSoilproperties[soil_type[hh]][3] > 0.0)
+                    if (Volumetric_option[hh])  // 04/14/2020
+                        fallstat[hh] = Volumetric_rechr[hh] / SetSoilproperties[soil_type[hh]][3] * 100000.0; // ie 100*1000
+                    else
+                        fallstat[hh] = Volumetric[hh] / SetSoilproperties[soil_type[hh]][3] * 100000.0; // ie 100*1000
+                else
+                    fallstat[hh] = 1.0;
+
+                fallstat_V[hh] = fallstat[hh];
+            }
         }
-        if(X < 0.0)
-          X = 0.0;
-        else if(X > 1.0){
-          X = 1.0;
-          const_cast<double *> (Si)[hh] = X;
-          Si_V[hh] = Si[hh];
-        }
-      }
-
-      if(fallstat[hh]){
-        if(SetSoilproperties[soil_type[hh]][3] > 0.0)
-          fallstat[hh] = Volumetric[hh]/SetSoilproperties[soil_type[hh]][3]*100000.0; // ie 100*1000
-        else
-          fallstat [hh] = 1.0;
-
-        fallstat_V[hh] = fallstat[hh];
-      }
     }
-  }
 }
--- release1.1/src/modules/coremodules/ClassVolumetric.h
+++ release1.1/src/modules/coremodules/ClassVolumetric.h
@@ -9,21 +9,25 @@ ClassVolumetric(string Name, string Version = "undefined", LMODULE Lvl = LMODULE
 
 // declared parameters
 const double *soil_Depth{ NULL };
-double *fallstat{ NULL };
-const double *Si{ NULL };
 const double *Si_correction{ NULL };
 const double *soil_moist_max{ NULL };
+const double *soil_rechr_max{ NULL };
 const long *soil_type{ NULL };
 const long *set_fallstat{ NULL };
-//const long *fallstat_Si;
+const long* Volumetric_option{ NULL };
+
 
 // declared variables
 double *Volumetric{ NULL };
+double *Volumetric_rechr{ NULL };
 double *fallstat_V{ NULL };
 double *Si_V{ NULL };
+double *fallstat{ NULL };
+double *Si{ NULL };
 
 // variable inputs
 const double *soil_moist{ NULL };
+const double *soil_rechr{ NULL };
 
 void decl(void);
 void init(void);
