38ba3adacb40917977435ae7682b3e2a19496f89
jhs507 <jhs507@mail.usask.ca>
2021-03-24 14:31:37 -0600

    Removed extra call to form create in the gcc based execution start up.

--- release1.1/src/gcc/main.cpp
+++ release1.1/src/gcc/main.cpp
@@ -6,7 +6,6 @@
 int main(int argc, char *argv[])
 {
     CRHMmain * m = new CRHMmain();
-    //m->FormCreate(); Form create is run in the constructor so it doesn't need to be called here. 
     m->DoPrjOpen(argv[1], "");
     m->RunClick ();
 }
