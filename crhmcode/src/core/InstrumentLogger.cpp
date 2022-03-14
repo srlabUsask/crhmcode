131af68d677505f528cf88d878418dbc12b2384e
jhs507 <jhs507@mail.usask.ca>
2022-03-14 14:37:22 -0600

    Adding InstrumentLogger.cpp/h

---
+++ crhmcode/src/core/InstrumentLogger.cpp
@@ -0,0 +1,36 @@
+#include "InstrumentLogger.h"
+#include <iostream>
+
+InstrumentLogger* InstrumentLogger::s_instance = 0;
+
+InstrumentLogger::InstrumentLogger()
+{
+    try
+    {
+        auto logger = spdlog::basic_logger_mt("instrumentLogger", "callLog.log");
+        this->instrumentLogger = logger;
+    }
+    catch (const spdlog::spdlog_ex& ex)
+    {
+        std::cout << "Log initialization failed for instrument logger: " << ex.what() << std::endl;
+        exit(1);
+    }
+}
+
+
+InstrumentLogger* InstrumentLogger::instance()
+{
+    if (!s_instance)
+        s_instance = new InstrumentLogger;
+    return s_instance;
+}
+
+std::shared_ptr<spdlog::logger> InstrumentLogger::get_instrument_logger()
+{
+    return instrumentLogger;
+}
+
+void InstrumentLogger::log_instrument_log(std::string msg)
+{
+    this->instrumentLogger->info(msg);
+}
