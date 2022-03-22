962fc5a78e634e6d80c97752c28b23a9ae123a2c
jhs507 <jhs507@mail.usask.ca>
2022-03-22 11:58:55 -0600

    Added logger pattern line to remove extranious output.

--- crhmcode/src/core/InstrumentLogger.cpp
+++ crhmcode/src/core/InstrumentLogger.cpp
@@ -9,6 +9,7 @@ InstrumentLogger::InstrumentLogger()
     {
         auto logger = spdlog::basic_logger_mt("instrumentLogger", "callLog.log");
         this->instrumentLogger = logger;
+        this->instrumentLogger->set_pattern("%v");
     }
     catch (const spdlog::spdlog_ex& ex)
     {
