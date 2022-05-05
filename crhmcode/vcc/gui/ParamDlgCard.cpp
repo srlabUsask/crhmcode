5117f725ff0f92f75b02f5acfc92eecc47c9029f
jhs507 <jhs507@mail.usask.ca>
2022-05-05 12:07:19 -0600

    Added ParamDlgCard class which handles the rendering of Parameter cards.

---
+++ crhmcode/vcc/gui/ParamDlgCard.cpp
@@ -0,0 +1,140 @@
+#include "ParamDlgCard.h"
+
+
+ParamDlgCard::ParamDlgCard(std::string testStr, CWnd* pParent /*=NULL*/ )
+	: CDialog(ParamDlgCard::IDD, pParent)
+{
+	Create(ParamDlgCard::IDD,pParent);
+	this->scroll_position = 0;
+	this->pane_height = 0;
+	this->text = testStr;
+}
+
+
+void ParamDlgCard::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+	DDX_Control(pDX, ID_PARAM_HELP, this->param_label);
+}
+
+
+BEGIN_MESSAGE_MAP(ParamDlgCard, CDialog)
+	ON_WM_VSCROLL()
+	ON_WM_MOUSEWHEEL()
+	ON_WM_SIZE()
+END_MESSAGE_MAP()
+
+
+BOOL ParamDlgCard::OnInitDialog()
+{
+	CDialog::OnInitDialog();
+	
+	// save the original size
+	GetWindowRect(original_rectangle);
+
+	// initial scroll position
+	scroll_position = 0; 
+
+	
+	return TRUE;
+}
+
+void ParamDlgCard::SetValues()
+{
+	CString infoText(this->text.c_str());
+
+	SetDlgItemText(ID_PARAM_HELP, (LPCTSTR)infoText);
+}
+
+void ParamDlgCard::OnVScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar)
+{
+	int nDelta;
+	int nMaxPos = original_rectangle.Height() - pane_height;
+
+	switch (nSBCode)
+	{
+	case SB_LINEDOWN:
+		if (scroll_position >= nMaxPos)
+			return;
+
+		nDelta = min(max(nMaxPos/20,5),nMaxPos-scroll_position);
+		break;
+
+	case SB_LINEUP:
+		if (scroll_position <= 0)
+			return;
+		nDelta = -min(max(nMaxPos/20,5),scroll_position);
+		break;
+	case SB_PAGEDOWN:
+		if (scroll_position >= nMaxPos)
+			return;
+		nDelta = min(max(nMaxPos/10,5),nMaxPos-scroll_position);
+		break;
+	case SB_THUMBTRACK:
+	case SB_THUMBPOSITION:
+		nDelta = (int)nPos - scroll_position;
+		break;
+
+	case SB_PAGEUP:
+		if (scroll_position <= 0)
+			return;
+		nDelta = -min(max(nMaxPos/10,5),scroll_position);
+		break;
+	
+		 default:
+		return;
+	}
+	scroll_position += nDelta;
+	SetScrollPos(SB_VERT,scroll_position,TRUE);
+	ScrollWindow(0,-nDelta);
+	CDialog::OnVScroll(nSBCode, nPos, pScrollBar);
+}
+
+
+void ParamDlgCard::OnSize(UINT nType, int cx, int cy)
+{
+	CDialog::OnSize(nType, cx, cy);
+
+	pane_height = cy;
+
+	SCROLLINFO si{};
+	si.cbSize = sizeof(SCROLLINFO);
+	si.fMask = SIF_ALL; 
+	si.nMin = 0;
+	si.nMax = original_rectangle.Height();
+	si.nPage = cy;
+	si.nPos = 0;
+	SetScrollInfo(SB_VERT, &si, TRUE); 	
+
+}
+
+
+BOOL ParamDlgCard::OnMouseWheel(UINT nFlags, short zDelta, CPoint pt)
+{
+	int nMaxPos = original_rectangle.Height() - pane_height;
+
+	if (zDelta<0)
+	{
+		if (scroll_position < nMaxPos)
+		{
+			zDelta = min(max(nMaxPos/20,5),nMaxPos-scroll_position);
+
+			scroll_position += zDelta;
+			SetScrollPos(SB_VERT,scroll_position,TRUE);
+			ScrollWindow(0,-zDelta);
+		}
+	}
+	else
+	{
+		if (scroll_position > 0)
+		{
+			zDelta = -min(max(nMaxPos/20,5),scroll_position);
+
+			scroll_position += zDelta;
+			SetScrollPos(SB_VERT,scroll_position,TRUE);
+			ScrollWindow(0,-zDelta);
+		}
+	}
+	
+	return CDialog::OnMouseWheel(nFlags, zDelta, pt);
+}
---
+++ crhmcode/vcc/gui/ParamDlgCard.h
@@ -0,0 +1,76 @@
+#pragma once
+
+#include <afxwin.h>
+#include "../resource.h"
+
+#include <string>
+
+class ParamDlgCard : public CDialog
+{
+private:
+	/*
+	* Defines the resource assoicated with this class.
+	*/
+	enum { IDD = PARAMETERS_CARD };
+
+	
+
+	/*
+	* CRect object representing the original size of the inserted pane.
+	*/
+	CRect original_rectangle;
+
+	/**
+	* Tracks the scroll position of the pane.
+	*/
+	int	scroll_position;
+
+	/**
+	* Tracks the actual height of the pane
+	*/
+	int	pane_height;
+
+	std::string text;
+
+	
+
+public:
+	/*
+	* Standard constructor
+	*/
+	ParamDlgCard(std::string test, CWnd* pParent = NULL );
+
+	CEdit param_label;
+
+	void SetValues();
+
+protected:
+
+	/**
+	* Connect data layer to the view.
+	*/
+	virtual void DoDataExchange(CDataExchange* pDX);
+
+	/**
+	* Initalize the dialog
+	*/
+	virtual BOOL OnInitDialog();
+
+	/**
+	* Handles vertical scrolling by use of the scroll bar.
+	*/
+	afx_msg void OnVScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar);
+
+	/**
+	* Handles vertical scrolling by use of the scroll wheel.
+	*/
+	afx_msg BOOL OnMouseWheel(UINT nFlags, short zDelta, CPoint pt);
+
+	/**
+	* Handles resizing of the pane.
+	*/
+	afx_msg void OnSize(UINT nType, int cx, int cy);
+	
+	DECLARE_MESSAGE_MAP()
+
+};
