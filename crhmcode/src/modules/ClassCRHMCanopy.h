abda4ba1dfe7a305d9f8fc586159405b4c84d391
loganxingfang <xing.fang@usask.ca>
2023-11-14 16:59:54 -0700

    Update ClassCRHMCanopy.h
    Adding variable "pot_subl_cpy" to output "dimensionless canopy sublimation rate"

--- crhmcode/src/modules/ClassCRHMCanopy.h
+++ crhmcode/src/modules/ClassCRHMCanopy.h
@@ -65,6 +65,7 @@ double *cum_net_snow { NULL };
 double *net_p { NULL };
 double *intcp_evap { NULL };
 double *cum_intcp_evap { NULL };
+double *pot_subl_cpy { NULL };
 double *Subl_Cpy { NULL };
 double *cum_Subl_Cpy { NULL };
 double *cum_SUnload { NULL };
