bd18ce63c400dc6a9d355622d41bc34f5569ddde
loganxingfang <xing.fang@usask.ca>
2026-03-23 11:49:10 -0600

    Update ClassCRHMCanopyVectorBased.h
    Removed unused parameters “Surrounding_Ht”, "Gap_diameter", "CanopyClearing".

--- crhmcode/src/modules/ClassCRHMCanopyVectorBased.h
+++ crhmcode/src/modules/ClassCRHMCanopyVectorBased.h
@@ -99,12 +99,9 @@ const double *Z0snow { NULL };
 const double *Zref { NULL };
 const double *Zwind { NULL };
 const double *Zvent { NULL };
-const double *Surrounding_Ht { NULL };
-const double *Gap_diameter { NULL };
 const double *Alpha_c { NULL };
 const double *B_canopy { NULL };
 
-const long *CanopyClearing { NULL };
 const long *CanopyWindSwitchIP { NULL };
 
 void decl(void);
